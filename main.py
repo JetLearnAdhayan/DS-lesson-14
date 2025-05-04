b38667842b4c662ebd36f1dc3be185aac0492595
JetLearnAdhayan <adhayanraj9@gmail.com>
2025-05-04 12:11:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -21,25 +21,39 @@ data["State"].fillna(value = " ", inplace = True)# fillna fills empty values aut
 top10_confirmed = pd.DataFrame(data.groupby("Country")["Confirmed"].sum().nlargest(10).sort_values(ascending=False))
 
 fig1 = px.scatter(top10_confirmed,x = top10_confirmed.index, y = "Confirmed", size = "Confirmed", size_max = 120, color = top10_confirmed.index, title = "Top 10 Countries by Confirmed cases")
-fig1.write_html("Fig1.html", auto_open = True)#opens file in browser
+fig1.write_html("Fig1.html", auto_open=True)#opens file in browser
 
 top10_recovered = pd.DataFrame(data.groupby("Country")["Recovered"].sum().nlargest(10).sort_values(ascending=False))
 fig2 = px.scatter(top10_recovered,x=top10_recovered.index, y="Recovered", size= "Recovered", size_max= 120,color=top10_recovered.index, title="Top 10 Countries by Recovered Cases")
-fig2.write_html("Fig2_html", auto_open = True)
+fig2.write_html("Fig2_html", auto_open=True)
 
 top10_deaths = pd.DataFrame(data.groupby("Country")["Deaths"].sum().nlargest(10).sort_values(ascending=False))
 fig3 = px.scatter(top10_deaths,x = top10_deaths.index,y="Deaths",size="Deaths", size_max=120,color=top10_deaths.index,title="Top 10 Countries by Deaths")
-fig3.write_html("Fig3_html", auto_open = True)
+fig3.write_html("Fig3_html", auto_open=True)
 
 top10_active = pd.DataFrame(data.groupby("Country")["Active"].sum().nlargest(10).sort_values(ascending=False))
 fig4 = px.scatter(top10_active, x=top10_deaths.index,y= "Active",size="Active",size_max=120,color=top10_active.index,title="Top 10 Countries by Active Cases" )
-fig4.write_html("Fig3_html",auto_open = True)
+fig4.write_html("Fig3_html",auto_open=True)
 
 top10_death = pd.DataFrame(data.groupby("Country")["Deaths"].sum().nlargest(10).sort_values(ascending=False))
 fig5 = px.bar(top10_death,x = "Deaths", y = top10_death.index, height=600,color = "Deaths" , orientation="h",color_continuous_scale=["deepskyblue","red"], title="Top 10 Death Cases Counties")
-fig5.write_html("Fig5.html", auto_open = True)
+fig5.write_html("Fig5.html", auto_open=True)
 
+#homework
+top10_confirmed = pd.DataFrame(data.groupby("Country")["Confirmed"].sum().nlargest(10).sort_values(ascending=False))
+fig6 = px.bar(top10_confirmed, x="Confirmed", y=top10_confirmed.index, height=600, color="Confirmed",
+              orientation="h", color_continuous_scale=["deepskyblue", "orangered"], title="Top 10 Confirmed Cases Countries")
+fig6.write_html("Fig6.html", auto_open=True)
 
+top10_recovered = pd.DataFrame(data.groupby("Country")["Recovered"].sum().nlargest(10).sort_values(ascending=False))
+fig7 = px.bar(top10_recovered, x="Recovered", y=top10_recovered.index, height=600, color="Recovered",
+              orientation="h", color_continuous_scale=["lightgreen", "darkgreen"], title="Top 10 Recovered Cases Countries")
+fig7.write_html("Fig7.html", auto_open=True)
+
+top10_active = pd.DataFrame(data.groupby("Country")["Active"].sum().nlargest(10).sort_values(ascending=False))
+fig8 = px.bar(top10_active, x="Active", y=top10_active.index, height=600, color="Active",
+              orientation="h", color_continuous_scale=["lightskyblue", "blue"], title="Top 10 Active Cases Countries")
+fig8.write_html("Fig8.html", auto_open=True)
 
 
 
